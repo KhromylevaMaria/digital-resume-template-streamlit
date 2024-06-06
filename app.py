c88f3cae1d79e13e26ff991f055ff03c2ddbe9eb
KhromylevaMaria <151607334+KhromylevaMaria@users.noreply.github.com>
2024-06-06 16:15:05 +0000

    CV_MariaKhromyleva

--- app.py
+++ app.py
@@ -6,57 +6,51 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV.pdf"
+resume_file = current_dir / "assets" / "CV_MariaKhromyleva.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
 
 # --- GENERAL SETTINGS ---
-PAGE_TITLE = "Digital CV | John Doe"
+PAGE_TITLE = "Digital CV | Maria Khromyleva"
 PAGE_ICON = ":wave:"
-NAME = "John Doe"
+NAME = "Maria Khromyleva"
 DESCRIPTION = """
-Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
+Data Analyst with over 15 years of IT experience, proficient in project management, and skilled in Python, SQL, and Tableau.
 """
-EMAIL = "johndoe@email.com"
+EMAIL = "khromyleva.maria@gmail.com"
 SOCIAL_MEDIA = {
-    "OnlyFans": "https://onlyfans.com/chocho_work",
-    "LinkedIn": "https://www.linkedin.com/in/pavel-chocholous/",
-    "GitHub":
-    "https://github.com/pavel242242/digital-resume-template-streamlit",
-    "Twitter": "https://twitter.com/pavel242",
+    "LinkedIn": "https://linkedin.com/in/mariakhromyleva",
+    "GitHub": "https://github.com/mariakhromyleva",
+    "Twitter": "https://twitter.com/mariakhromyleva"
 }
 PROJECTS = {
-    "🏆 Sales Dashboard - Comparing sales across three stores":
-    "https://youtu.be/Sb0A9i6d320",
-    "🏆 Income and Expense Tracker - Web app with NoSQL database":
-    "https://youtu.be/3egaMfE9388",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar":
-    "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel":
-    "https://pythonandvba.com/mytoolbelt/",
+    "🏆 Reporting System with Dashboards - Reduced report preparation from 1 day to 5 minutes":
+    "https://github.com/mariakhromyleva/reporting_system",
+    "🏆 Automated Accounting Reporting System for a 15-company holding - Shortened reporting time from 10 days to 2 days":
+    "https://github.com/mariakhromyleva/accounting_system",
 }
 SECTIONS = {
     "Experience": "Exp",
     "Tech stack": "Hard-skills",
-    "Career history": "CV",
+    "Education": "Education",
     "Portfolio": "Projects",
 }
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
-# --- LOAD CSS - Trochu crazy, ale neni to uplne nutny, jen priklad, co jde delat.. ---
+# --- LOAD CSS ---
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
-# --- LOAD PDF
+# --- LOAD PDF ---
 with open(resume_file, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
 
-# --- PROFIL PIC
+# --- PROFILE PIC ---
 profile_pic = Image.open(profile_pic)
 
 with st.sidebar:
-    my_radio = st.radio("Wow effect radio button", list(SECTIONS.keys()))
-    awesomeness_level = st.slider("Awesomeness leves", 0, 10, 0)
+    my_radio = st.radio("Select Section", list(SECTIONS.keys()))
+    awesomeness_level = st.slider("Rate my profile", 0, 10, 5)
 
 # --- HERO SECTION ---
 col1, col2 = st.columns(2, gap="small")
@@ -67,10 +61,10 @@ with col2:
     st.title(NAME)
     st.write(DESCRIPTION)
     st.download_button(
-        label=" 📄 Download Resume",
+        label=" 📄 Download CV",
         data=PDFbyte,
-        file_name=resume_file.name,
-        mime="application/octet-stream",
+        file_name="MariaKhromyleva_CV.pdf",
+        mime="application/pdf"
     )
     st.write("📫", EMAIL)
 
@@ -83,12 +77,11 @@ for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
 if my_radio == "Experience":
     # --- EXPERIENCE & QUALIFICATIONS ---
     st.write('\n')
-    st.subheader("Experience & Qulifications")
+    st.subheader("Experience & Qualifications")
     st.write("""
-    - ✔️ 7 Years expereince extracting actionable insights from data
-    - ✔️ Strong hands on experience and knowledge in Python and Excel
-    - ✔️ Good understanding of statistical principles and their respective applications
-    - ✔️ Excellent team-player and displaying strong sense of initiative on tasks
+    - ✔️ Project development and implementation of IT systems and KPIs
+    - ✔️ Led IT teams and infrastructure maintenance for 300 users
+    - ✔️ Expertise in data analysis and business process automation
     """)
 
 if my_radio == "Tech stack":
@@ -96,49 +89,22 @@ if my_radio == "Tech stack":
     st.write('\n')
     st.subheader("Hard Skills")
     st.write("""
-    - 👩‍💻 Programming: Python (Scikit-learn, Pandas), SQL, VBA
-    - 📊 Data Visulization: PowerBi, MS Excel, Plotly
-    - 📚 Modeling: Logistic regression, linear regression, decition trees
-    - 🗄️ Databases: Postgres, MongoDB, MySQL
+    - 👩‍💻 Programming: Python, SQL
+    - 📊 Data Visualization: Tableau
+    - 🗄️ Project Management: Jira, Microsoft Project, Confluence
     """)
 
-if my_radio == "Career history":
-    # --- WORK HISTORY ---
+if my_radio == "Education":
+    # --- EDUCATION ---
     st.write('\n')
-    st.subheader("Work History")
-    st.write("---")
-
-    # --- JOB 1
-    st.write("🚧", "**Senior Data Analyst | Ross Industries**")
-    st.write("02/2020 - Present")
+    st.subheader("Education")
     st.write("""
-    - ► Used PowerBI and SQL to redeﬁne and track KPIs surrounding marketing initiatives, and supplied recommendations to boost landing page conversion rate by 38%
-    - ► Led a team of 4 analysts to brainstorm potential marketing and sales improvements, and implemented A/B tests to generate 15% more client leads
-    - ► Redesigned data model through iterations that improved predictions by 12%
-    """)
-
-    # --- JOB 2
-    st.write('\n')
-    st.write("🚧", "**Data Analyst | Liberty Mutual Insurance**")
-    st.write("01/2018 - 02/2022")
-    st.write("""
-    - ► Built data models and maps to generate meaningful insights from customer data, boosting successful sales eﬀorts by 12%
-    - ► Modeled targets likely to renew, and presented analysis to leadership, which led to a YoY revenue increase of $300K
-    - ► Compiled, studied, and inferred large amounts of data, modeling information to drive auto policy pricing
-    """)
-
-    # --- JOB 3
-    st.write('\n')
-    st.write("🚧", "**Data Analyst | Chegg**")
-    st.write("04/2015 - 01/2018")
-    st.write("""
-    - ► Devised KPIs using SQL across company website in collaboration with cross-functional teams to achieve a 120% jump in organic traﬃc
-    - ► Analyzed, documented, and reported user survey results to improve customer communication processes by 18%
-    - ► Collaborated with analyst team to oversee end-to-end process surrounding customers' return data
+    - 🎓 Northern (Arctic) Federal University, Degree in Forest Engineering
+    - 🎓 Continuous learning through online courses in Python, SQL, Tableau
     """)
 
 if my_radio == "Portfolio":
-    # --- Projects & Accomplishments ---
+    # --- PROJECTS & ACCOMPLISHMENTS ---
     st.write('\n')
     st.subheader("Projects & Accomplishments")
     st.write("---")
@@ -146,7 +112,6 @@ if my_radio == "Portfolio":
         st.write(f"[{project}]({link})")
 
 for i in range(awesomeness_level + 1):
-    awesomeness_level = i
     with st.sidebar:
         st.write("🎉" * i)
     time.sleep(0.2)
